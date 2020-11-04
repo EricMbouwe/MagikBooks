bd556044818c09a5b8b2d86a44b7308423992fd9
ericmbouwe <ericmbouwe@yahoo.fr>
2020-11-04 22:20:08 +0100

    category filter component added

---
+++ src/components/CategoryFilter.js
@@ -0,0 +1,26 @@
+const CategoryFilter = () => {
+  const categories = [
+    'All',
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi',
+  ];
+
+  return ( 
+    <div>
+      <select name="category" id="category">
+        {categories.map(category => (
+          <option value={category} key={category}>
+            {category}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+ 
+export default CategoryFilter;
--- src/reducers/filter.js
+++ src/reducers/filter.js
@@ -1,6 +1,8 @@
 import { CHANGE_FILTER } from "../actions";
 
-export const filterReducer = (state = 'ALL', action) => {
+const filter = 'All'
+
+export const filterReducer = (state = filter, action) => {
   switch (action.type) {
     case CHANGE_FILTER:
       return 
