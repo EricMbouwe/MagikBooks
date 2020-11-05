e2c19180bb331e2cb8465d9b8de325a4dde82412
ericmbouwe <ericmbouwe@yahoo.fr>
2020-11-05 14:05:59 +0100

    app height chanaged

--- src/components/Book.js
+++ src/components/Book.js
@@ -3,12 +3,25 @@ import PropTypes from 'prop-types';
 
 const Book = ({ book, handleDelete }) => (
   <>
-    <td>{book.id}</td>
-    <td>{book.title}</td>
-    <td>{book.category}</td>
-    <td>
-      <button type="button" onClick={() => handleDelete(book)}>Remove Book</button>
-    </td>
+    <div className='book-infos'>
+      <div className='infos'></div>
+      <div className='actions'></div>
+    </div>
+
+    <div className='book-status'>
+      <div className='loading-icon'></div>
+      <div className='percentage'>
+        <p>64%</p>
+        <p>Completed</p>
+      </div>
+    </div>
+
+    <div className='progress-status'>
+      <p>CURENT CHAPTER</p>
+      <p>Chapter 17</p>
+      <button type='button'>UPDATE PROGRESS</button>
+    </div>
+    <button type="button" onClick={() => handleDelete(book)}>Remove Book</button>
   </>
 );
 
